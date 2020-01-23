50e8be40d2c6a0bb38f20b25fb8216d666648ae5
Emrah Yergin <emrahyergin@gmail.com>
2020-01-23 22:33:05 +0300

    fix

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
- apiKey: API_CLARIFAI
+ apiKey: 'f446875b81604720ba41c48223173936'
 });
 
 const handleApiCall = (req, res) => {
