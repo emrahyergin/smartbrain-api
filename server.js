e67b9e5d66d9a9a285987abb976880f994fae3d2
Emrah Yergin <emrahyergin@gmail.com>
2020-01-23 14:18:58 +0300

    database issues

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(db.users) })
+app.get('/', (req, res) => { res.send('it is working!') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { reg.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
