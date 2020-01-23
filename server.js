d3a1ad3ae5906d27d2232d1464e80d78ed7741cf
Emrah Yergin <emrahyergin@gmail.com>
2020-01-23 15:08:18 +0300

    adding database url

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-opaque-23200',
-    user : 'junior',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
